2f9e751e60adc342e26ef5193d7dbd20c721df3c
Sithiz <Joshklaas1@gmail.com>
2019-01-26 15:18:34 -0600

    Made head way on basic set up

---
+++ vampire_app/model/VampireModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose') 	
+
+const VampireSchema = mongoose.Schema({
+
+	  name: {
+		  type :String,
+		  required : true
+	  },
+
+    hair_color : {
+      type : String,
+      default : 'Blonde'
+    },
+    eye_color : String,
+    dob : {
+    	type : Date,
+    	required : true
+    },
+    loves : [String],
+    location : String,
+    gender : {
+  	  type: String,
+      required : true
+    },
+    victims :{
+      type : Number,
+      default : 1
+    }
+})
+
+const VampireModel = mongoose.model('Vampire', VampireSchema)
+
+module.exports =  VampireModel
+
+
